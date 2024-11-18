40030a989bfd86b5f4d48148e8cb74a43227e340
oscar <oscar.moreno@cti.espol.edu.ec>
2024-11-17 21:15:18 -0500

    add moderations anbd querys

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,13 +11,16 @@ app.post("/events", (req, res) => {
 
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
-  });
+  }); // post
   axios.post("http://localhost:4001/events", event).catch((err) => {
     console.log(err.message);
-  });
+  }); //comments
   axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log(err.message);
-  });
+  }); // query
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  }); // moderation
   res.send({ status: "OK" });
 });
 
