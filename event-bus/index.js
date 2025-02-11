7cb3dcdb176e195be0be24f02ff269831a89e301
oscar <oscar.moreno@cti.espol.edu.ec>
2025-02-10 19:31:10 -0500

    updating addresses

--- event-bus/index.js
+++ event-bus/index.js
@@ -15,15 +15,15 @@ app.post("/events", (req, res) => {
   axios.post("http://post-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   }); // post
-  //axios.post("http://localhost:4001/events", event).catch((err) => {
-  //  console.log(err.message);
-  //}); //comments
-  //axios.post("http://localhost:4002/events", event).catch((err) => {
-  //  console.log(err.message);
-  //}); // query
-  //axios.post("http://localhost:4003/events", event).catch((err) => {
-  //  console.log(err.message);
-  //}); // moderation
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
+    console.log(err.message);
+  }); //comments
+  axios.post("http://query-srv :4002/events", event).catch((err) => {
+    console.log(err.message);
+  }); // query
+  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
+    console.log(err.message);
+  }); // moderation
   res.send({ status: "OK" });
 });
 
